4ee41ba364a542a2d9546c4d0cc60a0761c8e27a
unknown <hossamhassan013@gmail.com>
2023-02-16 07:39:16 +0200

    verify

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2855644548984078"
           crossorigin="anonymous"
         ></script>
+        <meta
+          name="google-site-verification"
+          content="lq1wuARJxA0OumwA6ULgrXB6O88FMlADh5oCKkfmKZE"
+        />
       </Head>
       <body className="bg-gray-100">
         <Main />
